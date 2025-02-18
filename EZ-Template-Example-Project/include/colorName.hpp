02cfa4a4b1d19750e84c5bc375933a1dacb30e5b
nightwolf3140 <nightwolf3140@gmail.com>
2025-02-18 10:43:07 -0500

    started recompiling codebase for ez-template

---
+++ EZ-Template-Example-Project/include/colorName.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern Drive chassis;
+
+/* List of colors needed to be defined
+(define as constants so they are protected memory)
+colorRed
+colorGreen
+colorBlack
+colorWhite
+
+*/
---
+++ EZ-Template-Example-Project/include/subsystems.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern Drive chassis;
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+inline pros::Optical leftColor();
+inline pros::Optical rightColor();
+// inline pros::Motor intake(1);
+// inline pros::adi::DigitalIn limit_switch('A');
